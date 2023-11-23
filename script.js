604fff35db83ca843a3a844281723817366a7c6d
Augusto Baroni Simionato <72254418+AugustoBSimionato@users.noreply.github.com>
2023-11-23 11:07:24 -0300

    feat: adding warn

--- script.js
+++ script.js
@@ -4,4 +4,23 @@ function mostrarIngresso() {
 
   var mainContent = document.getElementById("main-content");
   mainContent.style.display = "block";
+
+  var numeroIngresso = Math.floor(Math.random() * 9000) + 1000;
+
+  atualizarNumeroIngresso(numeroIngresso);
+}
+
+function atualizarNumeroIngresso(numero) {
+  var numeroIngressoElement = document.getElementById("numero-ingresso");
+  if (numeroIngressoElement) {
+    numeroIngressoElement.textContent = numero;
+  }
+}
+
+function fecharAviso() {
+  var aviso = document.getElementById("aviso");
+  aviso.style.display = "none";
+
+  var introContainer = document.getElementById("intro-container");
+  introContainer.style.display = "block";
 }
