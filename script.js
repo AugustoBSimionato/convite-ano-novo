ef1afb5caaaeea2fd9a46ba5f31777ae9d5c075f
Augusto Baroni Simionato <72254418+AugustoBSimionato@users.noreply.github.com>
2023-11-23 10:43:14 -0300

    removing comments

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function mostrarIngresso() {
-  // Esconde o conteúdo de introdução
   var introContainer = document.getElementById("intro-container");
   introContainer.style.display = "none";
 
-  // Mostra o conteúdo principal
   var mainContent = document.getElementById("main-content");
   mainContent.style.display = "block";
 }
