65695254b613a000f345297c7f3e8cc129926d3b
Augusto Baroni Simionato <72254418+AugustoBSimionato@users.noreply.github.com>
2023-11-23 10:04:30 -0300

    feat: adding button to catch a invitation

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarIngresso() {
+  // Esconde o conteúdo de introdução
+  var introContainer = document.getElementById("intro-container");
+  introContainer.style.display = "none";
+
+  // Mostra o conteúdo principal
+  var mainContent = document.getElementById("main-content");
+  mainContent.style.display = "block";
+}
