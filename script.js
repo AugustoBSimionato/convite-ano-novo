eca243fba12c021c20c2bc62856ad16fd4ba63ff
Augusto Baroni Simionato <72254418+AugustoBSimionato@users.noreply.github.com>
2023-11-30 17:58:16 -0300

    feat: adding family names to convite

--- script.js
+++ script.js
@@ -5,9 +5,25 @@ function mostrarIngresso() {
   var mainContent = document.getElementById("main-content");
   mainContent.style.display = "block";
 
+  // Obtenha os nomes da família do campo de texto
+  var nomesFamiliaInput = document.getElementById("nomesFamilia");
+  var nomesFamilia = nomesFamiliaInput.value;
+
+  // Gere um número aleatório
   var numeroIngresso = Math.floor(Math.random() * 9000) + 1000;
 
+  // Atualize o número do ingresso no convite
   atualizarNumeroIngresso(numeroIngresso);
+
+  // Adicione os nomes da família ao convite
+  adicionarNomesFamilia(nomesFamilia);
+}
+
+function adicionarNomesFamilia(nomes) {
+  var nomesFamiliaElement = document.getElementById("nomes-familia");
+  if (nomesFamiliaElement) {
+    nomesFamiliaElement.textContent = "Família: " + nomes;
+  }
 }
 
 function atualizarNumeroIngresso(numero) {
