a9fb30e838505e475b1c93e2004f06a075ad3d3b
Augusto Baroni Simionato <72254418+AugustoBSimionato@users.noreply.github.com>
2023-12-01 18:45:27 -0300

    fix: deleting unnecessary lines of code on html

--- script.js
+++ script.js
@@ -5,24 +5,20 @@ function mostrarIngresso() {
   var mainContent = document.getElementById("main-content");
   mainContent.style.display = "block";
 
-  // Obtenha os nomes da família do campo de texto
   var nomesFamiliaInput = document.getElementById("nomesFamilia");
   var nomesFamilia = nomesFamiliaInput.value;
 
-  // Gere um número aleatório
   var numeroIngresso = Math.floor(Math.random() * 9000) + 1000;
 
-  // Atualize o número do ingresso no convite
   atualizarNumeroIngresso(numeroIngresso);
 
-  // Adicione os nomes da família ao convite
   adicionarNomesFamilia(nomesFamilia);
 }
 
 function adicionarNomesFamilia(nomes) {
   var nomesFamiliaElement = document.getElementById("nomes-familia");
   if (nomesFamiliaElement) {
-    nomesFamiliaElement.textContent = "Família: " + nomes;
+    nomesFamiliaElement.textContent = "Convidados: " + nomes;
   }
 }
 
